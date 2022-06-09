a406aad25134dbd2b2d7f67d3ffd2aed08162955
xieby1 <xieby1@outlook.com>
2022-06-10 02:11:04 +0800

    upgrade to v3

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({file: "fullscreen.js"});
+chrome.action.onClicked.addListener(function(tab) {
+  chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ["fullscreen.js"]
+  });
 });
